12558cdd080c8ccd4a993e4c7c80280276b3fd02
JARO1213 <98063294+JARO1213@users.noreply.github.com>
2023-03-09 16:30:15 -0500

    merge and quicksort

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -1,4 +1,7 @@
 'use strict'
+
+const { merge } = require("@11ty/eleventy/src/TemplateData");
+
 // No cambies los nombres de las funciones.
 
 function quickSort(array) {
@@ -6,14 +9,49 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if (array <= 1) return array;
+  let pivoteI = Math.floor(Math.random()*array.length);
+  let pivote = array[pivoteI]
+  let right =[];
+  let left = [];
+  let equals = [];
+  
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] === pivote){
+      equals.push(array[i]);
+    }else if( array[i] < pivote){
+      left.push(array[i]);
+    }else if (array[i] > pivote){
+      right.push(array[i]);
+    }
+      }
+   return quickSort (left).concat(equals).concat(quickSort(right))
 }
+
+
+
+  
 
 function mergeSort(array) {
   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length <= 1) return array;
+let pivotIndex = Math.floor(array.length/2)
+let pivote = array[pivotIndex];
+let left = [];
+let right= [];
+for (let index = 0; index < array.length; index++) {
+  if(index === pivotIndex){
+    continue;
+  }else if (array[index] < pivote){
+     left.push(array[index]);
+    } else {
+     right.push(array[index]);
+    }
+ }
+return mergeSort(left).concat([pivote], mergeSort(right))
 
   //okey probemos ahora
 }
